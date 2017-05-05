a0840e447e79c88bfb6a77244e619ebf39626940
nattesharan <nattesharan@gmail.com>
2017-05-05 18:34:36 +0530

    Added bootstrap jumbotron template.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return 'Under Construction'
+    return render_template('home.html')
 if __name__ == '__main__':
     app.run(port=int('3000'),debug=True)
