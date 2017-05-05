f0a4065c39e48039faa0ed66db7c2013aa617bf8
nattesharan <nattesharan@gmail.com>
2017-05-05 22:56:06 +0530

    Added flask_login

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask,render_template
+from flask_login import LoginManager,login_required
 app = Flask(__name__)
+login_manager = LoginManager(app)
 @app.route('/')
 def home():
     return render_template('home.html')
+@app.route('/account')
+@login_required
+def account():
+    return 'You are logged in'
 if __name__ == '__main__':
     app.run(port=int('3000'),debug=True)
