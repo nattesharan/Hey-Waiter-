aa89cce5bf3875b2a7f6fa44fe37ad0d80520015
nattesharan <nattesharan@gmail.com>
2017-05-06 15:07:52 +0530

    added salt for passwords during registration and during login and  also added password validation

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask_login import LoginManager,login_required
 from flask_login import login_user,logout_user
 from mockdbhelper import MockDBHelper as DBhelper
 from user import User
+from passwordhelper import passHelper
+ph = passHelper()
 DB = DBhelper()
 app = Flask(__name__)
 app.secret_key = 'cMlCVgyfbFYllvKDM5QzBc5en09yydjez+FcA3m55o02GeKZGWY3c6/z6AdBZHMUd7QErw2lKgUv2SXNk2Y2WM6156lmwCcYijs'
@@ -18,8 +20,8 @@ def account():
 def login():
     email = request.form.get("email")
     password = request.form.get("password")
-    user_password = DB.get_user(email)
-    if user_password and user_password == password:
+    stored_user = DB.get_user(email)
+    if stored_user and ph.validate_password(password,stored_user['salt'],stored_user['hashed']):
         user = User(email)
         login_user(user, remember = True)
         return redirect(url_for('account'))
@@ -32,6 +34,19 @@ def loaduser(user_id):
 @app.route('/logout')
 def logout():
     logout_user()
+    return redirect(url_for('home'))
+@app.route('/register',methods = ['POST'])
+def register():
+    email = request.form['email']
+    passwd = request.form['password']
+    passwd1 = request.form['password2']
+    if not passwd == passwd1:
+        return redirect(url_for('home'))
+    if DB.get_user(email):
+        return redirect(url_for('home'))
+    salt = ph.get_salt()
+    hashed = ph.get_hash(passwd + salt)
+    DB.add_user(email,salt,hashed)
     return redirect(url_for('home'))
 if __name__ == '__main__':
     app.run(port=int('3000'),debug=True)
